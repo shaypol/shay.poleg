796e29b2ae95e20a6cf2b28bab94c94f433086c5
Shay Poleg <shaypoleg@gmail.com>
2021-12-04 16:13:40 +0200

    Relocation of phone icon and email validation

---
+++ web-project-g7/contact.js
@@ -0,0 +1,6 @@
+function validateEmail() {
+  const inpObj = document.getElementById("id1");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = inpObj.validationMessage;
+  }
+}
