d66f7c2550001ef5aadb54575a1a57b58a49889e
Shay Poleg <59873590+shaypol@users.noreply.github.com>
2021-12-12 21:10:56 +0200

    Delete web-project-g7 directory

--- web-project-g7/contact.js
+++
@@ -1,7 +0,0 @@
-function validateEmail() {
-  const inpObj = document.getElementById("id1");
-  if (!inpObj.checkValidity()) 
-  {
-    document.getElementById("demo").innerHTML = inpObj.validationMessage;
-  }
-}
